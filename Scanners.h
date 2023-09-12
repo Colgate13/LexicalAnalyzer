c36e4aee7f464b19671328a5c99bafceb1235920
Gabriel Barros <gabreilbarros13@gmail.com>
2023-09-12 01:57:52 -0300

    Scanners

---
+++ Scanners.h
@@ -0,0 +1,11 @@
+#ifndef SCANNERS_H
+#define SCANNERS_H
+
+unsigned short int isDigit(char c);
+unsigned short int isLetter(char c);
+unsigned short int isLetterLowCase(char c);
+unsigned short int isLetterUpperCase(char c);
+unsigned short int isSpace(char c);
+unsigned short int isOperator(char c);
+
+#endif
